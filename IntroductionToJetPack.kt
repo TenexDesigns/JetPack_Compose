1d097985c4591dc70e874e6adc565cbc18cf9c43
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-05 18:34:21 +0300

    Update IntroductionToJetPack.kt

--- IntroductionToJetPack.kt
+++ IntroductionToJetPack.kt
@@ -6,19 +6,38 @@ and when ever the underlying data changes the framework automaticaly recalls thi
 THIS MEANS THAT 
 From now on if you want to build your app with jetpack compose you ar not going to need any xml files for declaring any ui content
 Instead you are going to build your UI using  @Composable functions.
-
 Composable functions are highly usable
+
+XML is not yet depricated . You can still build app using fragments and xml or even combine that with jetpack compose
+
 
 JETPACK COMPOSE 
 Also supportsMVVM out of the box
 This means that it will be easier to arrange our code into smaller chunks
 
 
+LIFE CYCLE 
+
+step 1               
+
+Enter the composition
+
+
+Step 2
+
+Recompose ) or more times  { composable}
+
+Step 3
+
+Leave the composition
 
 
 
 
-XML is not yet depricated . You can still build app using fragments and xml or even combine that with jetpack compose
+
+
+
+
 
 
 
