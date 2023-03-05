1a4b5f75ddd8598164939b41dd4caa2647671449
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-05 18:02:57 +0300

    Create IntroductionToJetPack.kt

---
+++ IntroductionToJetPack.kt
@@ -0,0 +1,39 @@
+JETPACKCOMPOS - Is a modern ui toolkit designed to simplyfy ui development and it combines reactive programing modl with the power of kotlin
+Jetpack is fully declarative - Meaning you describe your ui by calling a series of functions that transorm data into your y=ui hierachy.
+and when ever the underlying data changes the framework automaticaly recalls this functions updating the view hierachy for you.
+
+
+THIS MEANS THAT 
+From now on if you want to build your app with jetpack compose you ar not going to need any xml files for declaring any ui content
+Instead you are going to build your UI using  @Composable functions.
+
+Composable functions are highly usable
+
+JETPACK COMPOSE 
+Also supportsMVVM out of the box
+This means that it will be easier to arrange our code into smaller chunks
+
+
+
+
+
+
+XML is not yet depricated . You can still build app using fragments and xml or even combine that with jetpack compose
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
