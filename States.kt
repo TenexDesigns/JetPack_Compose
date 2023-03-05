09321c51ff958d6edd33befe7cc6b343967d50f8
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-05 19:34:57 +0300

    Create States.kt

---
+++ States.kt
@@ -0,0 +1,27 @@
+STATE is any value that can change over time.
+
+The ui is immutble after it has been drawen but what you can change is the state of your ui.
+
+You can only update your ui by chaging the state
+
+STATEFUL AND STATELESS COMPOSABLES
+Avoid using stateful composables and do try using stateless composables by handling the stat using state hoistimng
+sTATE HOISTING.
+
+EVENT 
+ Notifys a part of a program that something has hapeened.
+
+
+
+view model
+         ^ 
+ |state  |event
+ |       |
+ v       
+first screen()
+         ^ 
+ |state  |event
+ |       |
+ v       
+
+screen content()
