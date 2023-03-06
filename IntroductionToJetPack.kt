2a882408de92e15c34b2cdf62ab31c435f920b1c
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-06 14:43:03 +0300

    Update IntroductionToJetPack.kt

--- IntroductionToJetPack.kt
+++ IntroductionToJetPack.kt
@@ -86,11 +86,93 @@ fun DefaultPreview() {
   To create items in colum layout you need to call the coloum composable function.
   This colum is a container . This container we can set ist specs within the parenthesis e.g
   The column accepis parameters within its paraenthesis . 
-  This parameters can be 
-  Column( modifier = Modefier.height(500.dp)  // This sets the height of this column container
-                             .width(200.dp) // This sets the width of this container
+  This parameters can be modifier, horizontalAlignmemnt,vertical arrangememnt
+ 
+ modifier             - Deals with the specs of the container e.gmodifier = Modifier.height(500.dp)
+                                                                                    .width(500.dp)
+                                                                                    .background(MaterialTheme.colors.error)
+ 
+ horizontalAlignmemnt - Deals with where the items in the colunm are put or aligned  on the horizontal axixi.e either on the start ,middle or end .We use the Alignmemnt to set their position
+ verticalArrangement  - Deals with how items are spread out or arrangaded on the vertical axis. The items can either be arranged in a spaceBetweeen or spaceEvenly or spaceAroud or center or top or bottom of the vertical axix.
+  
+ HERES THE EXAMPLE 
+                     // This sets the height of this column container
+ Column( modifier = Modefier.height(500.dp) ,horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Top ){
+                  // Here is where we describe our item containers using the surface composable function  
+   e.g            surface( // Here are parameters that descibe this container){}
+ }
+ 
+ 
+ 
+
+HERE IS AN EXAMPLE OF A SURFACE
+    
+    
+    The surface() ,receives parameters that help shape the contaner . The paramaters are e.g
+    Modifier - Sets the specs for the surface container e.g with ,height 
+    color - Give the color for the container
+    and many more parameters
+    
+        Surface(
+        modifier = Modifier
+            .width(200.dp)
+            .height(50.dp)
+            .weight(weight),
+        color = color
+
+    ) {}
+        
+        
+        A surface is one item and we can set many more surfaces within a row or column
+    
+    e.g 
+ column(modifier = Modifier.fillMaxSize()){
+            // First Item
+            Surface(modifier = Modifier.width(20.dp)
+                                       .height(200.dp)
+                                       .weight(3f), // The weigh is used to grow/expandd the surface cotainer based on the fraction it is give 
 
 
+                   color = MaterialTheme.colors.secondary
+            ) {}
+            // Second item 
+            Surface(modifier = Modifier.width(20.dp)
+                                       .height(200.dp)
+                                       .weight(3f), // The weigh is used to grow/expandd the surface cotainer based on the fraction it is give 
+
+
+                   color = MaterialTheme.colors.secondary
+            ) {}
+    
+    
+    This is one way of creating many Items in a column or row
+  How ever you can create a composable function of the type of surcafe or row or column that will take your desired shape.
+  And you can populate your column or row layout withba s many of this items as yo want by calling the function that created it 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+                             .width(200.dp)  ) // This sets the width of this container
+    
+    
+    
+    
 
 
 
