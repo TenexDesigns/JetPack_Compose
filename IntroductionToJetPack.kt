c4e238aff33e7ce5a6f48bbc3d1a4d10f34f858c
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-06 14:57:15 +0300

    Update IntroductionToJetPack.kt

--- IntroductionToJetPack.kt
+++ IntroductionToJetPack.kt
@@ -95,8 +95,14 @@ fun DefaultPreview() {
  horizontalAlignmemnt - Deals with where the items in the colunm are put or aligned  on the horizontal axixi.e either on the start ,middle or end .We use the Alignmemnt to set their position
  verticalArrangement  - Deals with how items are spread out or arrangaded on the vertical axis. The items can either be arranged in a spaceBetweeen or spaceEvenly or spaceAroud or center or top or bottom of the vertical axix.
   
+ ******* IN ROW THE ABOVE TWO ARE DIFFFFERRENT 
+ // In row ,it HorizontalArrangment and VerticalAlignment
+ IN ROW 
+ HorizontalArrangment - Deals with how items are spread out or arrangaded on the horizontal axis. The items can either be arranged in a spaceBetweeen or spaceEvenly or spaceAroud or center or start or end of the vertical axix.
+ VerticalAlignment  - Deals with where the row with the items is put on  the verticla axix . Either top ,bottom or center.
+ 
  HERES THE EXAMPLE 
-                     // This sets the height of this column container
+                     // This sets the height of this column container            
  Column( modifier = Modefier.height(500.dp) ,horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Top ){
                   // Here is where we describe our item containers using the surface composable function  
    e.g            surface( // Here are parameters that descibe this container){}
@@ -148,6 +154,57 @@ HERE IS AN EXAMPLE OF A SURFACE
     This is one way of creating many Items in a column or row
   How ever you can create a composable function of the type of surcafe or row or column that will take your desired shape.
   And you can populate your column or row layout withba s many of this items as yo want by calling the function that created it 
+  
+  
+  //This is the callable surface that will populate your cloumn when called in a column layout
+  @Composable
+fun ColumnScope.custom(weight:Float,color: Color){
+
+    Surface(
+        modifier = Modifier
+            .width(200.dp)
+            .height(50.dp)
+            .weight(weight),
+        color = color
+
+    ) {}
+
+
+}
+
+  //This is the callable surface that will populate your row when called in a column layout
+  
+  @Composable
+fun RowScope.custom(weight:Float,color: Color){
+
+    Surface(
+        modifier = Modifier
+            .width(200.dp)
+            .height(50.dp)
+            .weight(weight),
+        color = color
+
+    ) {}
+
+
+}
+
+
+THEN THIS IS HOW YOU CALL THE SURFCA CLLLABLE METHODS
+  
+   column(modifier = Modifier.fillMaxSize()){
+    
+                        custom(weight = 2f, color = MaterialTheme.colors.secondary)
+                        custom(weight = 2f, color = MaterialTheme.colors.primary)
+   }
+           
+    
+  
+  
+  
+  
+  
+
     
     
     
@@ -168,7 +225,6 @@ HERE IS AN EXAMPLE OF A SURFACE
     
     
     
-                             .width(200.dp)  ) // This sets the width of this container
     
     
     
