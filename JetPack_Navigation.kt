eb8a7e19a7f4de30661bc39c7ca53221b16edaf2
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-11 08:59:45 +0300

    Update JetPack_Navigation.kt

--- JetPack_Navigation.kt
+++ JetPack_Navigation.kt
@@ -125,6 +125,14 @@ var navController  = rememberNavController()
                 HomeScreen()
               
               }
+              
+              SECOND SCREEN
+//But for this screen to be displayed we have to add a click listener  oto the oar of the first screen that we want to be clicked so that we can navigate to this screen
+             composable(route = Screen.DEtails.route){
+                
+                DetailsScreen()
+              
+              }
             
             
             
@@ -135,6 +143,186 @@ var navController  = rememberNavController()
           
           
           }
+
+          
+          
+          TO NAVIGATE FROM HOME SCREEN TO DETAILS SCREEN WE HAVE TO ADD THE CLICKABLE LISTENR TO THE PART WE WANT USER TO CLICK TO NVALGATE TO THE E SCREEN
+
+            
+            https://accounts.google.com/b/0/AddMailService
+//In our home screen we give it a parameter of navController//
+ // We pass this navController  in our NavHost file ehere we dclared this c=scrrens           
+@Composable
+fun HomeScreen(navController:NavController) {
+
+    Box( Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+
+    ){
+        Text(
+            modifier = Modifier.clickable(
+//Here we receive the clickable modifier
+               // we use the navigate method of the navcontroller. This navigate receives many methods ,but one of them is the route
+               // this route is used to move from one screen to another
+                navController.navigate( route = Screen.Detail.route)
+            
+            )
+            text="Home",
+            color= Color.Red,
+            fontSize = MaterialTheme.typography.h3.fontSize,
+            fontWeight = FontWeight.Bold
+        )
+
+    }
+}
+
+
+
+         @Composable
+          fun SetUpNavHost(
+            navController:NavHostController
+          
+          ){
+            
+            NavHost(
+              navControler = navController,
+              startDestination = Screen.Home.route ){
+                
+                
+                Composable(){
+                    HomeScreen(navControler)// Here is where we pass the navController
+                
+                
+                }
+            
+            
+            
+            
+            }
+            
+            
+            
+            
+            WE DO THE SAME FOR US TO NAVIGATE FROM THE DETAILS TO THE HOMESCREEN
+              @Composable
+fun DetailsScreen(navController:NavController) {
+
+    Box( Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+
+    ){
+        Text(
+            modifier = Modifier.clickable(
+//Here we receive the clickable modifier
+               // we use the navigate method of the navcontroller. This navigate receives many methods ,but one of them is the route
+               // this route is used to move from one screen to another
+                navController.navigate( route = Screen.Home.route)
+            
+            ),
+            text="Home",
+            color= Color.Red,
+            fontSize = MaterialTheme.typography.h3.fontSize,
+            fontWeight = FontWeight.Bold
+        )
+
+    }
+}
+
+
+
+// Here is where we pass the navController to the detailsScreen
+          
+           @Composable
+          fun SetUpNavHost(
+            navController:NavHostController
+          
+          ){
+            
+            NavHost(
+              navControler = navController,
+              startDestination = Screen.Home.route ){
+                
+                
+                Composable(){
+                    DeatilsScreen(navControler)// Here is where we pass the navController
+                
+                
+                }
+            
+            
+            
+            
+            }
+            
+            
+            HOWEVER WHEN WERE ON THE DETAILS SCREEN ,WE CAN USE THE BACK BUTTON TO GO BACK TO THE SCREEN .
+              HOWEVER WHEN WE CLICK THE BACKBUTTON ,WE W=ARE TAKEN TO THE DETAILS SCREEN INSTR=EAD OF OUTOF THE APP.
+              tHIS IS BECAUSE THE DTAILS SCRENN IS NOT REMOB=VED FROM THE BACKSTACK
+              
+              
+              //Use this when you just want to pop the backstack
+              navController.popBackStack()
+              
+              //Use this when you want to pass arguments in to the back screen
+              
+              navController.navigate(Screen.Home.route){
+                  
+                  popUpTo(Screen.Home.route){
+                      
+                      inclusive = true
+                  
+                  }
+              
+              }
+              
+              
+              exmple
+@Composable
+fun DetailsScreen(navController:NavController) {
+
+    Box( Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+
+    ){
+        Text(
+            modifier = Modifier.clickable(
+//Here we receive the clickable modifier
+               // we use the navigate method of the navcontroller. This navigate receives many methods ,but one of them is the route
+               // this route is used to move from one screen to another
+                navController.popBackStack()          =----------------------------> or 
+                
+                    or 
+                              
+              navController.navigate(Screen.Home.route){
+                  
+                  popUpTo(Screen.Home.route){
+                      
+                      inclusive = true
+                  
+                  }
+              
+              }
+              
+            )
+            
+            ),
+            text="Home",
+            color= Color.Red,
+            fontSize = MaterialTheme.typography.h3.fontSize,
+            fontWeight = FontWeight.Bold
+        )
+
+    }
+}
+              
+            
+            
+            
+              
+              
+              
+              
+
             
             
           
