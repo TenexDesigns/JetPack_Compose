e32ecfb7f850490e9d0043b91eb2f4ae3bfc0155
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-14 16:12:12 +0300

    Create Retrofit1.kt

---
+++ Retrofit1.kt
@@ -0,0 +1,84 @@
+There are app we make that are native and can be used as they have all that they rquire.
+But  for oue apps to be more expanded it has to connect to the intenert and aceess web services such as e.g apis giving images,
+Our app sends this http reuest and the receiving web server send the http response.
+How ever handling sending this https request and receinging the http response in our app is very hard.
+That is where our retefit libray comes in .
+
+
+Retrofit is used to  handle the retrieve, update and delete of  the data from web services.
+
+                                   http response                    
+client  <--->   retrofit   <--->                  <------->   web services(webs erver)
+                                   http request
+                   
+
+
+HERES WHAT WE WILL LEARN
+
+
+HTTP fundamentals
+Web services - create your own local web server
+Retrofit - To get your app online.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
