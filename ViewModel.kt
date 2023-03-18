3ed56faab96dc2cb4715412cba41fbf7ef52890e
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-18 10:17:27 +0300

    Update ViewModel.kt

--- ViewModel.kt
+++ ViewModel.kt
@@ -72,6 +72,434 @@ https://medium.com/androiddevelopers/viewmodels-a-simple-example-ed5ac416317e
 
 
 
+At Google I/O 2017, the Android Framework team introduced a new set of Architecture Components, 
+one of which deals with this exact rotation issue.
+
+VIEW MODEL
+_______________________________________________________________________________________________________________
+
+
+The ViewModel class is designed to hold and manage UI-related data in a life-cycle conscious way. 
+This allows data to survive configuration changes such as screen rotations.
+
+This post is the first in a series exploring the ins and outs of ViewModel. In this post I’ll:
+
+
+Explain the basic need ViewModels fulfill
+Solve the rotation issue by changing the Court-Counter code to use a ViewModel
+Take a closer look at ViewModel and UI Component association
+
+
+
+The underlying problem
+
+The underlying challenge is that the Android Activity lifecycle has a lot of states and
+a single Activity might cycle throug
+h these different states [: onCreate(), onStart(), onResume(), onPause(), onStop(), and onDestroy().]
+many times due to configuration changes.
+
+
+
+As an Activity is going through all of these states, you also might have transient UI data that you need to keep in memory. 
+I’m going to define transient UI data as data needed for the UI. Examples include data the user enters, data generated during runtime,
+or data loaded from a database.
+This data could be bitmap images, a list of objects needed for a RecyclerView or, in this case, a basketball score.
+
+
+
+ Instead of having a variable like scoreTeamA within the Activity, and therefore tied to all the whims of the Activity lifecycle,
+what if that data was stored somewhere else, outside of the Activity? This is the purpose of the ViewModel class.
+
+
+In the diagram below, you can see the lifecycle of an Activity which undergoes a rotation and then is finally finished.
+The lifetime of the ViewModel is shown next to the associated Activity lifecycle. Note that ViewModels can be easily used 
+with both Fragments and Activities, which I’ll call UI controllers. 
+This example focuses on Activities.
+
+
+The diagranm is in this repisortory ,it is called done.png
+
+
+
+The ViewModel exists from when the you first request a ViewModel (usually in the onCreate the Activity)
+until the Activity is finished and destroyed.
+onCreate may be called several times during the life of an Activity, such as when the app is rotated, but the ViewModel survives throughout.
+
+
+
+A very simple example
+
+There are three steps to setting up and using a ViewModel:
+
+1.Separate out your data from your UI controller( Fragments and Activities) by creating a class that extends ViewModel
+2.Set up communications between your ViewModel and your UI controller
+3.Use your ViewModel in your UI controller
+
+
+
+
+Step 1: Create a ViewModel class
+
+Note: To create a ViewModel, you’ll first need to add the correct lifecycle dependency. See how here in this repisortory in the file callled AllDependcies.
+
+In general, you’ll make a ViewModel class for each screen in your app. This ViewModel class will hold all of the data 
+associated with the screen and have getters and setters for the stored data. This separates the code to display the UI, 
+which is implemented in your Activities and Fragments,
+from your data, which now lives in the ViewModel. So, let’s create a ViewModel class for the one screen in Court-Counter:
+
+
+class ScoreViewModel : ViewModel() {
+    // Tracks the score for Team A
+    var scoreTeamA = 0
+
+    // Tracks the score for Team B
+    var scoreTeamB = 0
+}
+
+
+
+
+
+Step 2: Associate the UI Controller and ViewModel
+Your UI controller (aka Activity or Fragment) needs to know about your ViewModel. 
+This is so your UI controller can display the data and update the data when UI interactions occur, such as pressing a button to increase
+a team’s score in Court-Counter.
+
+ViewModels should not, though, hold a reference to Activities, Fragments, or Contexts.** Furthermore,
+ViewModels should not contain elements that contain references to UI controllers, such as Views, since this will create an indirect 
+reference to a Context.
+
+The reason you shouldn’t store these objects is that ViewModels outlive your specific UI controller instances 
+— if you rotate an Activity three times, you have just created three different Activity instances, but you only have one ViewModel.
+
+
+With that in mind, let’s create this UI controller/ViewModel association. You’ll want to create a member variable for your ViewModel in the UI Controller. Then in onCreate, you should call:
+
+ViewModelProviders.of(<Your UI controller>).get(<Your ViewModel>.class)
+
+
+
+In the case of Court-Counter, this looks like:
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+   super.onCreate(savedInstanceState);
+   setContentView(R.layout.activity_main);
+   mViewModel = ViewModelProviders.of(this).get(ScoreViewModel.class);
+   // Other setup code below...
+}
+
+**Note: There’s one exception to the “no contexts in ViewModels” rule. Sometimes you might need an Application context 
+(as opposed to an Activity context) for use with things like system services. Storing an Application context in
+a ViewModel is okay because an Application context is tied to the Application lifecycle. 
+This is different from an Activity context, which is tied to the Activity lifecycle.
+In fact, if you need an Application context, you should extend AndroidViewModel which is simply a ViewModel that includes
+an Application reference.
+
+
+
+
+
+
+
+
+
+
+
+
+
+Step 3: Use the ViewModel in your UI Controller
+To access or change UI data, you can now use the data in your ViewModel.
+Here’s an example of the new onCreate method and a method for updating the score by adding one point to team A:
+
+// The finished onCreate method
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+   super.onCreate(savedInstanceState);
+   setContentView(R.layout.activity_main);
+   mViewModel = ViewModelProviders.of(this).get(ScoreViewModel.class);
+   displayForTeamA(mViewModel.scoreTeamA);
+   displayForTeamB(mViewModel.scoreTeamB);
+}
+
+// An example of both reading and writing to the ViewModel
+public void addOneForTeamA(View v) {
+   mViewModel.scoreTeamA = mViewModel.scoreTeamA + 1;
+   displayForTeamA(mViewModel.scoreTeamA);
+}
+
+
+
+Pro tip: ViewModel also works very nicely with another Architecture Component, LiveData, which I won’t be exploring deeply in this series. 
+The added bonus here of using LiveData is that it’s observable: it can trigger UI updates when the data changes.
+You can learn more about LiveData here.
+
+A closer look at ViewModelsProviders.of
+The first time the ViewModelProviders.of method is called by MainActivity, it creates a new ViewModel instance.
+When this method is called again, which happens whenever onCreate is called, it will return the pre-existing ViewModel associated
+with the specific Court-Counter MainActivity. This is what preserves the data.
+
+This works only if you pass in the correct UI controller as the first argument.
+While you should never store a UI controller inside of a ViewModel, the ViewModel class does keep track of the associations
+between ViewModel and UI controller instance behind the scenes, using the UI controller you pass in as the first argument.
+
+ViewModelProviders.of(<THIS ARGUMENT>).get(ScoreViewModel.class);
+This allows you to have an app that opens a lot of different instances of the same Activity or Fragment,
+but with different ViewModel information. Let’s imagine if we extended our Court-Counter example to have the scores
+for multiple basketball games. The games are presented in a list, and then clicking on a game in the list opens a 
+screen that looks like our current MainActivity, but which I’ll call GameScoreActivity.
+
+For every different game screen you open, if you associate the ViewModel and GameScoreActivity in onCreate,
+it will create a different ViewModel instance. If you rotate one of these screens, the connection to the same ViewModel is maintained.
+
+
+All of this logic is done for you by calling ViewModelProviders.of(<Your UI controller>).get(<Your ViewModel>.class).
+So as long as you pass in the correct instance of a UI controller, it just works.
+
+
+
+
+
+A final thought: ViewModels are very nifty for separating out your UI controller code from the data which fills your UI. That said, they are not a cure all for data persistence and saving app state. In the next
+post I’ll explore the subtler interactions of the Activity lifecycle with ViewModels and how ViewModels compare to onSaveInstanceState.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
