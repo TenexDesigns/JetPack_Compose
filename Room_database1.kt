33ea5c678f5c7dd0402f9ed0e13dc7fd792258f3
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-19 16:35:07 +0300

    Update Room_database1.kt

--- Room_database1.kt
+++ Room_database1.kt
@@ -145,6 +145,207 @@ But this should be enough to get you started.
 
 
 
+ROOM DATABASE TUTORIAL
+_____________________________________________________________________________________________________________________________________________________
+
+
+
+
+
+
+
+
+
+
+
+Room is a persistence library that provides an abstraction layer over SQLite database.
+It is a part of the Android Jetpack library that simplifies the process of working with SQLite databases in Android. 
+Room provides an easy-to-use interface to perform database operations, while also ensuring compile-time checks for SQL 
+statements and allowing you to write more concise, readable code.
+
+In Android Jetpack Compose, Room database can be used to store data that needs to be persisted across application sessions. 
+Here are the basic steps to use Room database in Android Jetpack Compose:
+
+
+
+Add Room dependency to your project: You can add the Room dependency to your project by adding the following line to your
+app-level build.gradle file:
+
+implementation "androidx.room:room-runtime:<version>"
+_____________________________________________________________________________________________________________________________________________________
+
+
+Create an Entity: An Entity is a data class that represents a table in your database. You can define an Entity class by
+annotating it with the @Entity annotation, and providing a table name and a primary key.
+For example, heres an Entity class that represents a User table:
+
+@Entity(tableName = "users")
+data class User(
+    @PrimaryKey val id: Int,
+    val name: String,
+    val age: Int
+)
+
+
+_____________________________________________________________________________________________________________________________________________________
+
+Create a DAO: A Data Access Object (DAO) is an interface that defines the methods to interact with the database. 
+You can define a DAO by annotating it with the @Dao annotation, and then defining methods for inserting, updating, and 
+deleting data from the database.
+For example, heres a DAO interface that defines methods for working with the User table:
+
+@Dao
+interface UserDao {
+    @Query("SELECT * FROM users")
+    fun getAllUsers(): List<User>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertUser(user: User)
+
+    @Delete
+    fun deleteUser(user: User)
+}
+
+
+_____________________________________________________________________________________________________________________________________________________
+
+
+
+Create a Room database: A Room database is created by extending the RoomDatabase class and defining the entities and DAOs that it will use. 
+You can define a Room database class by annotating it with the @Database annotation, and providing a list of entities and a list of DAOs.
+For example, heres a Room database class that defines the User Entity and the UserDao:
+
+@Database(entities = [User::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun userDao(): UserDao
+}
+
+
+_____________________________________________________________________________________________________________________________________________________
+
+
+Use Room database in your Composable: Finally, you can use the Room database in your Composable by creating an instance of 
+the database and calling the methods defined in the DAO.
+Heres an example of how you can use the Room database to display a list of users in a Composable:
+
+
+@Composable
+fun UserList(db: AppDatabase) {
+    val users = remember { db.userDao().getAllUsers() }
+    LazyColumn {
+        items(users) { user ->
+            Text(text = user.name)
+        }
+    }
+}
+
+Thats a basic overview of how you can use Room database in Android Jetpack Compose. Of course, 
+there are many more advanced features of Room, such as defining relationships 
+between entities, using LiveData or Flow to observe changes in the database, and using migrations to update the database schema.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
