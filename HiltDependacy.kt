27f65b020474c133e49a3189caa13167fba686ab
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-21 08:00:17 +0300

    Update HiltDependacy.kt

--- HiltDependacy.kt
+++ HiltDependacy.kt
@@ -1,4 +1,4 @@
-HILT DEPENDECY INJECTION
+HILT DEPENDECY INJECTION      ---> The dependecies for this hilt are at the bootom of the page
 
 There are two types of hilt dependecy injections
 
@@ -293,11 +293,93 @@ ________________________________________________________________________________
 
 
 
+THESE ARE THE DEPENDCIES TO ENABLE HILT ,AND THE SPLASH API
+___________________________________________________________________________________________________________________________________________________
 
 
 
+         plugins {
+    id 'com.android.application'
+    id 'org.jetbrains.kotlin.android'
+    id 'dagger.hilt.android.plugin'
+    id 'kotlin-kapt'
+}
+
+android {
+    namespace 'com.example.tenexapp'
+    compileSdk 33
+
+    defaultConfig {
+        applicationId "com.example.tenexapp"
+        minSdk 24
+        targetSdk 33
+        versionCode 1
+        versionName "1.0"
+
+        testInstrumentationRunner "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary true
+        }
+    }
+
+    buildTypes {
+        release {
+            minifyEnabled false
+            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+        }
+    }
+    compileOptions {
+        sourceCompatibility JavaVersion.VERSION_1_8
+        targetCompatibility JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = '1.8'
+    }
+    buildFeatures {
+        compose true
+    }
 
 
+    packagingOptions {
+        resources {
+            excludes += '/META-INF/{AL2.0,LGPL2.1}'
+        }
+    }
+}
+
+dependencies {
+
+    implementation 'androidx.core:core-ktx:1.9.0'
+    implementation "androidx.compose.ui:ui:1.3.3"
+    implementation "androidx.compose.material:material:1.3.1"
+    implementation "androidx.compose.ui:ui-tooling-preview:1.3.3"
+    implementation 'androidx.lifecycle:lifecycle-runtime-ktx:2.6.0'
+    implementation 'androidx.activity:activity-compose:1.6.1'
+    testImplementation 'junit:junit:4.13.2'
+    androidTestImplementation 'androidx.test.ext:junit:1.1.5'
+    androidTestImplementation 'androidx.test.espresso:espresso-core:3.5.1'
+    androidTestImplementation "androidx.compose.ui:ui-test-junit4:1.3.3"
+    debugImplementation "androidx.compose.ui:ui-tooling:1.3.3"
+
+    // Splash API // Used to implement spash screen on our android application
+    implementation 'androidx.core:core-splashscreen:1.1.0-alpha01'
+
+    // Navigation Compose
+    implementation("androidx.navigation:navigation-compose:2.6.0-alpha07")
+
+    // Pager and Indicators - Accompanist  // Allows us to implement on boarding screeens
+    implementation "com.google.accompanist:accompanist-pager:1.2.1"
+    implementation "com.google.accompanist:accompanist-pager-indicators:1.2.1"
+
+    // DataStore Preferences  // Saves a boolean value ,and it allows us to load the spalsh screen the first time the user install the application
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+
+    // Dagger - Hilt
+    implementation "com.google.dagger:hilt-android:2.38.1"
+    kapt "com.google.dagger:hilt-android-compiler:2.38.1"
+    kapt 'androidx.hilt:hilt-compiler:1.0.0'
+    implementation 'androidx.hilt:hilt-navigation-compose:1.0.0'
+}
 
 
 
