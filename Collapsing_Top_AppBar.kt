baae27097b7f1233a443b6235181909a7a10337f
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-22 15:53:19 +0300

    Create Collapsing_Top_AppBar.kt

---
+++ Collapsing_Top_AppBar.kt
@@ -0,0 +1,6 @@
+
+
+In this post, I’ll show you how to implement a collapsing toolbar with parallax effect in jetpack compose only (no third party library). 
+I’ll also show you how to implement curved motion using quadratic Bézier curve and simple maths 😉
+
+Read More Here  -> https://www.droidcon.com/2022/10/10/collapsing-toolbar-with-parallax-effect-and-curved-motion-in-jetpack-compose-%F0%9F%98%8E/
