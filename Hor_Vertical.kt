c67af029fabd0f0d308d569a5e0143009e7f0eef
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-26 20:53:58 +0300

    Create Hor_Vertical.kt

---
+++ Hor_Vertical.kt
@@ -0,0 +1,151 @@
+There is verticalAlignmemnt and
+
+
+   Column(verticalArrangement = Arrangement.Bottom    // This puts all the items 
+                                            .Center
+                                            .top
+                                            .spaceEvenly
+                                            .spaceBetween
+                                            .Spacearound
+                                            ,
+                                            ,
+        horizontalAlignment = Alignment.CenterHorizontally  // This aligns the items on the center of the x axixs
+                                       .start // This puts the elemnts to the start of the x axix. This is the default value.
+                                       .end // This puts the elemnts to the end of the x axix
+                                       
+        
+        ){
+        
+        Box(modiifier = modifier.background = Grey)
+        Box(modiifier = modifier.background = Grey)
+        Box(modiifier = modifier.background = Grey)
+    }
+    
+    
+    
+
+    Row(
+        horizontalArrangement =  Arrangement.Center,
+        verticalAlignment =  Alignment.Bottom
+    ){
+
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
