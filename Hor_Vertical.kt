ac2c566701680ea3c0fdea9d57ad26a57a35b807
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-26 21:57:51 +0300

    Update Hor_Vertical.kt

--- Hor_Vertical.kt
+++ Hor_Vertical.kt
@@ -1,12 +1,12 @@
 There is verticalAlignmemnt and
 
 
-   Column(verticalArrangement = Arrangement.Bottom    // This puts all the items 
-                                            .Center
-                                            .top
-                                            .spaceEvenly
-                                            .spaceBetween
-                                            .Spacearound
+   Column(verticalArrangement = Arrangement.Bottom    // This puts all the items at the bootom of the y axis
+                                            .Center /// This arranges all the items on the center of the y axixx
+                                            .top   // This arranges all the items at the top of the y axixs
+                                            .spaceEvenly // This spreads the space betweeen the items evenly accroos the items on the y-axix  without free space on the first and last item.
+                                            .spaceBetween  // This putse space betweeen the items across the  y-axix
+                                            .Spacearound  // This spreads the space betweeen the items evenly accroos the items on the y-axix and its puts free space on first and last item.
                                             ,
                                             ,
         horizontalAlignment = Alignment.CenterHorizontally  // This aligns the items on the center of the x axixs
@@ -21,6 +21,27 @@ There is verticalAlignmemnt and
         Box(modiifier = modifier.background = Grey)
     }
     
+    
+    
+   Column(horizontalArrangement = Arrangement.Bottom    // This puts all the items at the bootom of the x axis
+                                            .Center /// This arranges all the items on the center of the x axixx
+                                            .top   // This arranges all the items at the top of the y-axixs
+                                            .spaceEvenly // This spreads the space betweeen the items evenly accroos the items on the x-axix  without free space on the first and last item.
+                                            .spaceBetween  // This putse space betweeen the items across the  x-axix
+                                            .Spacearound  // This spreads the space betweeen the items evenly accroos the items on the x-axix and its puts free space on first and last item.
+                                            ,
+                                            ,
+        horizontalAlignment = Alignment.CenterHorizontally  // This aligns the items on the center of the y axixs
+                                       .start // This puts the elemnts to the start of the y axix. This is the default value.
+                                       .end // This puts the elemnts to the end of the y axix
+                                       
+        
+        ){
+        
+        Box(modiifier = modifier.background = Grey)
+        Box(modiifier = modifier.background = Grey)
+        Box(modiifier = modifier.background = Grey)
+    }
     
     
 
