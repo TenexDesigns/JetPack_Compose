ec8e096362c920327d767ca4e3f2726c6d145a15
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-27 09:03:55 +0300

    Update Hor_Vertical.kt

--- Hor_Vertical.kt
+++ Hor_Vertical.kt
@@ -7,7 +7,7 @@ There is verticalAlignmemnt and
                                             .spaceEvenly // This spreads the space betweeen the items evenly accroos the items on the y-axix  without free space on the first and last item.
                                             .spaceBetween  // This putse space betweeen the items across the  y-axix
                                             .Spacearound  // This spreads the space betweeen the items evenly accroos the items on the y-axix and its puts free space on first and last item.
-                                            ,
+                                            .spacedby(12.dp)// This puts a custopm spacing between the components in the column
                                             ,
         horizontalAlignment = Alignment.CenterHorizontally  // This aligns the items on the center of the x axixs
                                        .start // This puts the elemnts to the start of the x axix. This is the default value.
