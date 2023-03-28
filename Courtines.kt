b0801033031e0d66360253bf76d09a88f0d2408c
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-28 12:29:08 +0300

    Create Courtines.kt

---
+++ Courtines.kt
@@ -0,0 +1,150 @@
+Couritinres help with runing code asynchronously.
+
+HERE IS A LIFE EXAMPLE
+
+In a hotel there is the main casheier called james who takes money from the buyers.
+How ever ther may be a buyer called john who has bought too 450 items and serving this buyer will take jammes a long time to finish and will cause a hold up or not make the line move. Behinf john ther are other buyes with 2 or 3 three items. 
+Now would it be fair to make this buyes with 2 or 3  items wait till John is finished being seviced.NO it is not fair  ,so what can be done is that another casher called emma is given to john to service his items
+while the main casier continoius on with the line so that there is no hold up.Emman can be servicing John making the running og the supermaket faster.
+
+
+Asynchronous code
+The above can be said for asyncrous code.
+Those actions that take a long time to be excuted can be performend on another  thread to avoid blocking the main thread and causing stagnation.
+Runing code asynchrounusly means that it does not block the ain thereda or is excuted on a background thread.
+
+
+Asynchronous code is code that does not block the execution thread while waiting for an operation to complete. 
+Instead, it schedules the operation and continues executing other tasks while waiting for the result. This allows the code to be more responsive and efficient, 
+especially when dealing with long-running operations such as network I/O, disk I/O or database queries.
+
+
+
+COURROTINES
+_______________________________________________________________________________________________________________________________________________
+
+Coroutines are a lightweight concurrency framework introduced in Kotlin that allow developers to write asynchronous, 
+non-blocking code in a more sequential and readable way. They simplify the process of performing long-running operations on the main thread, 
+such as network or disk I/O, without blocking the UI and leading to a more responsive application.
+
+Coroutines are essentially lightweight threads that can be launched within a coroutine scope,
+such as a function or a coroutine builder like "launch", "async", or "runBlocking".
+They can communicate with each other using channels, and can be cancelled at any time without affecting the entire process.
+
+Heres an example of using a coroutine to perform a network request:
+
+Noth--> This is the main thead // Dispachers.Main
+
+Thses are background threds
+// Dispachers.IO
+// Dispachers.Default
+// Dispachers.unconfined
+
+
+fun fetchUserData() {     
+  // Here we launch the  coroutines within a coroutine scope using a coroutine builder like launch ,or async or runBlocking
+    CoroutineScope(Dispatchers.IO).launch {// Then we change the code excution from the main thread to the IO threead usion the Dispachers>IO
+        // Perform a network request on a background thread
+        val result = makeNetworkRequest()
+
+        // Update the UI with the result on the main thread
+        withContext(Dispatchers.Main) {  //We use the withContect method to enable us to return To the main Thread .Here we return to the main thread using the Dispachers.Main
+            updateUI(result) // Here we send the result to the updata funnction afetr the code excution has been done on the background thread i.e Dispacher>io thread
+        }
+    }
+}
+
+
+In this example, we create a new coroutine scope using CoroutineScope(Dispatchers.IO) to indicate that the coroutine should be launched on a background thread. 
+Within the coroutine scope, we launch a new coroutine using the launch builder and perform a network request asynchronously using the makeNetworkRequest function.
+Once the network request is complete, we update the UI with the result using withContext(Dispatchers.Main) to switch back to the main thread.
+
+Heres another example of using a coroutine to perform a long-running computation:
+
+suspend fun doLongComputation(): Int {
+    return withContext(Dispatchers.Default) {
+        // Perform a long-running computation on a background thread
+        computeResult()
+    }
+}
+
+fun performComputation() {
+    CoroutineScope(Dispatchers.Main).launch {
+        // Start the computation on a background thread
+        val result = doLongComputation()
+
+        // Update the UI with the result on the main thread
+        updateUI(result)
+    }
+}
+
+
+In this example, we define a suspend function called doLongComputation that performs a long-running computation using computeResult() and returns the result.
+We use withContext(Dispatchers.Default) to indicate that the computation should be performed on a background thread.
+
+In performComputation, we launch a new coroutine on the main thread using CoroutineScope(Dispatchers.Main).launch. 
+We then call doLongComputation to start the computation on a background thread, and update the UI with the result using updateUI 
+on the main thread once the computation is complete.
+
+Overall, coroutines provide a simple and efficient way to write asynchronous code in Kotlin, without the need for callbacks,
+threads or complex synchronization mechanisms. They can help simplify code and improve performance by allowing long-running operations
+to be performed on a separate thread without blocking the main thread.
+
+
+
+
+__________________________________________________________________________________________________________________________________________________________
+
+
+In the above context, asynchronous code refers to code that doesnt block the execution thread while waiting for a long-running operation to complete.
+Instead, it allows the thread to continue executing other tasks while the long-running operation is being processed in the background.
+
+For example, in the first code sample I provided, the fetchUserData() function performs a network request using a coroutine launched on a background thread.
+The network request is an asynchronous operation that could take some time to complete, but instead of blocking the execution thread and causing the UI to freeze, 
+the coroutine allows the thread to continue running other tasks until the network request is complete.
+
+Similarly, in the second code sample, the performComputation() function launches a coroutine on the main thread to perform a long-running
+computation on a background thread using doLongComputation(). The computation is another example of an asynchronous operation 
+that doesnt block the UI and allows the thread to continue running other tasks until its complete.
+
+By using coroutines to write asynchronous code, we can avoid blocking the execution thread, improve the responsiveness of our application,
+and ensure a smoother user experience. Coroutines provide a clean and easy-to-read way to perform long-running operations in the background
+while keeping the main thread free to handle UI updates and user interactions.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
