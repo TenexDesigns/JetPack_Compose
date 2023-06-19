44b8bad9672cac516190a9519c6ce95b094dac2e
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-06-19 10:49:17 +0300

    Create Http_Methods.js

---
+++ Http_Methods.js
@@ -0,0 +1,33 @@
+Here is a list of common HTTP methods available:
+
+GET: Retrieves information from a specified resource. It is used to fetch data without modifying it (developer.mozilla.org).
+POST: Submits data to a specified resource for processing and/or storage. It is used to create new resources (developer.mozilla.org).
+PUT: Updates a specified resource with new data, replacing the existing data entirely. It is used to modify an existing resource (developer.mozilla.org).
+PATCH: Partially updates a specified resource with new data. It is used to apply partial modifications to a resource (developer.mozilla.org).
+DELETE: Deletes a specified resource. It is used to remove a resource from the server (developer.mozilla.org).
+OPTIONS: Retrieves information about the communication options available for a specified resource. It is used to describe the communication options for the target resource (developer.mozilla.org).
+HEAD: Retrieves information from a specified resource, similar to GET, but without the response body. It is used to obtain metadata about a resource without transferring the actual data (developer.mozilla.org).
+CONNECT: Establishes a network connection for use with a proxy, typically for use with HTTPS (developer.mozilla.org).
+TRACE: Retrieves a diagnostic representation of the request and response messages for a specified resource. It is used for diagnostic purposes (developer.mozilla.org).
+These HTTP methods allow you to perform various actions on resources, such as retrieving, creating, updating, and deleting data, as well as obtaining metadata and diagnostic information.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  ....
