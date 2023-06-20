135ea74c4584bfc4b78c7098c58cce4f2a0e67af
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-06-20 20:50:08 +0300

    Create Retrofit_1_Project_ERROR_HANDLING.js

---
+++ Retrofit_1_Project_ERROR_HANDLING.js
@@ -0,0 +1,181 @@
+We are Going to handle The retrofit errors in the view model
+
+
+
+  4.View_Model.kt:  --> This is where wefetch the data from the api and expose it so that it can be used by composable functions
+
+class PostViewModel : ViewModel() {
+    private val _postList = mutableStateListOf<PostResponse>()
+    var errorMessage: String by mutableStateOf("")
+    val postList: List<PostResponse>
+        get() = _postList
+
+fun getPostList() {
+    viewModelScope.launch {
+        val postService = ServiceBuilder.buildService(PostService::class.java)
+        try {
+            val response = postService.getPostList()
+            if (response.isSuccessful) {
+                // Handle successful response
+                    _postList.clear()
+                    _postList.addAll(response.body()!!)
+              
+            } else if (response.code() == 401){
+
+                val errorMessage = response.errorBody()?.string() // or use a custom parser for more complex error responses
+                // Log the error message or update the UI
+                Log.e("PostViewModel", "Error Your session has expired. Please Login again: $errorMessage")
+            }
+            
+            
+            else { // Application-level failure
+                    // Your status code is in the range of 300's, 400's and 500's
+                // Extract the error message from the response
+                val errorMessage = response.errorBody()?.string() // or use a custom parser for more complex error responses
+                // Log the error message or update the UI
+                Log.e("PostViewModel", "Error fetching posts: $errorMessage")
+            }
+        } catch (e: Exception) {
+            // Handle exceptions thrown during the network call
+            Log.e("PostViewModel", "Exception fetching posts: ${e.message}")
+        }
+    }
+}
+
+}
+
+
+
+
+
+
+More Explanantion
+*****************************************************************************************************************8888
+
+  To handle errors in Retrofit and log or display them in the context of the provided code, you can follow these steps:
+
+Check the success of the response in the getPostList() function in PostViewModel. If the response is not successful,
+    you can extract the error message and update the UI or log the error:
+fun getPostList() {
+    viewModelScope.launch {
+        val postService = ServiceBuilder.buildService(PostService::class.java)
+        try {
+            val response = postService.getPostList().await()
+            if (response.isSuccessful) {
+                // Handle successful response
+            } else {
+                // Extract the error message from the response
+                val errorMessage = response.errorBody()?.string() // or use a custom parser for more complex error responses
+                // Log the error message or update the UI
+                Log.e("PostViewModel", "Error fetching posts: $errorMessage")
+            }
+        } catch (e: Exception) {
+            // Handle exceptions thrown during the network call
+            Log.e("PostViewModel", "Exception fetching posts: ${e.message}")
+        }
+    }
+}
+To display the error message in the UI, you can create a mutable LiveData or State in your PostViewModel to store the error message and
+  update it when an error occurs. Then, observe this LiveData or State in your Display composable function and display the error message accordingly:
+In PostViewModel:
+
+val errorMessage = MutableLiveData<String?>()
+
+// Update the errorMessage in the error handling branches
+In Display composable function:
+
+@Composable
+fun Display() {
+    // Get a reference to the ViewModel
+    val postViewModel = viewModel<PostViewModel>()
+
+    // Observe the error message LiveData
+    val errorMessage by postViewModel.errorMessage.observeAsState(null)
+
+    // Fetch the data when the Display composable is launched
+    LaunchedEffect(Unit) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+...
