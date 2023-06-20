9683ffa4db7d5a3bb5a33d5d3825a525492d2c13
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-06-20 21:29:45 +0300

    Create Retrofit_!_List_of_Dependecies.js

---
+++ Retrofit_!_List_of_Dependecies.js
@@ -0,0 +1,49 @@
+Thses are the depencdeies I used In my retrofit app
+
+
+
+
+implementation 'androidx.compose.animation:animation:1.4.3'
+    implementation 'androidx.compose.ui:ui:1.4.3'
+    implementation 'androidx.compose.material:material:1.4.3'
+
+
+
+    implementation "com.google.accompanist:accompanist-systemuicontroller:0.27.0"
+
+
+    implementation("androidx.navigation:navigation-compose:2.6.0")
+    implementation "androidx.navigation:navigation-compose:2.7.0-beta01"
+    implementation 'androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1'
+    implementation 'androidx.lifecycle:lifecycle-livedata-ktx:2.6.1'
+    implementation "androidx.compose.ui:ui:1.4.3"
+    implementation "androidx.compose.material:material:1.4.3"
+    implementation "androidx.compose.ui:ui-tooling:1.4.3"
+    implementation "androidx.compose.runtime:runtime-livedata:1.4.3"
+    implementation "androidx.navigation:navigation-compose:2.7.0-beta01"
+
+
+
+    implementation 'androidx.core:core-ktx:1.10.1'
+    implementation platform('org.jetbrains.kotlin:kotlin-bom:1.8.0')
+    implementation 'androidx.lifecycle:lifecycle-runtime-ktx:2.6.1'
+    implementation 'androidx.activity:activity-compose:1.7.2'
+    implementation platform('androidx.compose:compose-bom:2022.10.00')
+    implementation 'androidx.compose.ui:ui'
+    implementation 'androidx.compose.ui:ui-graphics'
+    implementation 'androidx.compose.ui:ui-tooling-preview'
+    implementation 'androidx.compose.material3:material3'
+    implementation 'androidx.constraintlayout:constraintlayout:2.1.4'
+    testImplementation 'junit:junit:4.13.2'
+    androidTestImplementation 'androidx.test.ext:junit:1.1.5'
+    androidTestImplementation 'androidx.test.espresso:espresso-core:3.5.1'
+    androidTestImplementation platform('androidx.compose:compose-bom:2022.10.00')
+    androidTestImplementation 'androidx.compose.ui:ui-test-junit4'
+    debugImplementation 'androidx.compose.ui:ui-tooling'
+    debugImplementation 'androidx.compose.ui:ui-test-manifest'
+    implementation("io.coil-kt:coil-compose:2.4.0")
+
+    implementation 'com.squareup.retrofit2:retrofit:2.8.0'
+    implementation 'com.squareup.retrofit2:converter-gson:2.8.0'
+    implementation 'com.squareup.okhttp3:okhttp:5.0.0-alpha.1'
+    implementation 'com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.1'
